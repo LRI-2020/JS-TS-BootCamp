0994a89596b0fa8b8dc84d3da8bae930f14fae0c
LaylaR <layla.richard.2111@gmail.com>
2023-11-21 17:25:40 +0100

    budget calculator sass display expenses

---
+++ JS bootcamp - robMeril/BudgetCalculator/app.ts
@@ -0,0 +1,24 @@
+﻿import {displayExpenses} from "./displayExpenses.js";
+
+let expense1 = {
+    id:1,
+    title:"expense1",
+    amount:52
+}
+let expense2 = {
+    id:2,
+    title:"expense2",
+    amount:52
+}
+let expense3 = {
+    id:3,
+    title:"expense3",
+    amount:52
+}
+let myExpenses:any[] = [];
+myExpenses.push(expense1, expense2, expense3);
+
+localStorage.setItem("expenses",JSON.stringify(myExpenses));
+
+
+displayExpenses();
---
+++ JS bootcamp - robMeril/BudgetCalculator/expenseHtml.ts
@@ -0,0 +1,36 @@
+﻿
+let expenseHtmlTemp =`<div class="expenses_info mx-2">
+    <h4 class="expense_title"></h4>
+        <h5 class="amount"></h5>
+</div>
+<div class="expense_actions d-inline-flex">
+<button class="btn btn-light p-0 w-50 mx-1 updateBtn">
+<img class="update w-50" src="./img/update.svg" alt="updateIcon">
+    </button>                   
+    <button class="btn btn-light w-50 p-0 mx-1 deleteBtn">
+<img class="delete w-50" src="./img/delete.svg" alt="deleteIcon">
+    </button>
+ </div>`;
+
+function createHtmlExpense(id:string,title:string, amount:string) : HTMLDivElement{
+   
+    let expense = generateExpenseDiv();
+    let titleEl = expense.querySelector('.expense_title')! as HTMLTitleElement;
+    let amountEl = expense.querySelector('.amount')! as HTMLTitleElement;
+    let updateEl = expense.querySelector('.expense_actions>.updateBtn')!
+    updateEl.setAttribute('expense_id',id);    
+    let deleteEl = expense.querySelector('.expense_actions>.deleteBtn')!
+    deleteEl.setAttribute('expense_id',id);
+    titleEl.innerText = title;
+    amountEl.innerText = amount;
+    return expense; 
+}
+
+function generateExpenseDiv(): HTMLDivElement{
+    let expense = document.createElement('div');
+    expense.classList.add("expense","d-inline-flex","align-items-center","shadow","p-3", "my-2");
+    expense.innerHTML = expenseHtmlTemp;
+    return expense;
+}
+
+export {createHtmlExpense}
